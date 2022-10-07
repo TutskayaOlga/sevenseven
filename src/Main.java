ba611d265fe051fdf3541439b11c61474de40188
olgatut <lelka.tutskaya@yandex.ru>
2022-10-07 22:35:02 +0300

    Тестово

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        var voice = 10;
+        var voice1 = 15;
+        int table = voice - voice1;
+        int table1 = Math.abs(voice - voice1);
+        System.out.println(table1);
+    }
+}
